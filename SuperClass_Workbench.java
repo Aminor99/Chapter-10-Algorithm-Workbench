e918be8688cf9fab9a7ec998a7cc449c502c9b35
Asia Minor <asia.minor99@gmail.com>
2019-04-16 14:31:12 -0400

    Add files via upload

---
+++ SuperClass_Workbench.java
@@ -0,0 +1,14 @@
+//Name: Asia Minor
+//Date: 4-16-19
+//Purpose: Algortihm Workbench chapter 10 #4 
+//Write the statement that calls the superclass constructor and passes the arguments x, y, and z
+
+public class SuperClass_Workbench{
+
+   public int a,b,c;
+   
+   public SuperClass_Workbench(int a, int b, int c){
+   
+   }
+   
+}
---
+++ SuperClass_Workbench_Demo.java
@@ -0,0 +1,14 @@
+//Name: Asia Minor
+//Date: 4-16-19
+//Purpose: Algortihm Workbench chapter 10 #4 
+//Write the statement that calls the superclass constructor and passes the arguments x, y, and z
+
+public class SuperClass_Workbench_Demo extends SuperClass_Workbench{
+
+   public int x, y, z;
+   
+   public SuperClass_Workbench_Demo(int x, int y, int z){
+      super(x, y, z);
+   }
+   
+}
