0b704530205b3ecc4510a5c655d4a6a8748c72c6
Asia Minor <asia.minor99@gmail.com>
2019-04-16 14:41:40 -0400

    Add files via upload

---
+++ Pass_Value_Workbench.java
@@ -0,0 +1,12 @@
+//Name: Asia Minor
+//Date: 4-16-19
+//Purpose: Algortihm Workbench chapter 10 #5
+//write a super class that has the following method and have the subclass call the method and pass 10 as an argument
+
+public class Pass_Value_Workbench{
+
+   public static void acceptValue(int n){
+   
+      System.out.println("You passed " + n + " from the subclass.");
+   }
+}
---
+++ Pass_Value_Workbench_Demo.java
@@ -0,0 +1,15 @@
+//Name: Asia Minor
+//Date: 4-16-19
+//Purpose: Algortihm Workbench chapter 10 #5
+//write a super class that has the following method and have the subclass call the method and pass 10 as an argument
+
+public class Pass_Value_Workbench_Demo extends Pass_Value_Workbench{
+
+   public static void main (String [] args){
+   
+      acceptValue(10);
+      
+   }
+   
+}
+
