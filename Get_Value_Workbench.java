c44ecad47c6525cff59c653adaa9593861ee84fb
Asia Minor <asia.minor99@gmail.com>
2019-04-16 14:50:11 -0400

    Add files via upload

---
+++ Get_Value_Workbench.java
@@ -0,0 +1,12 @@
+//Name: Asia Minor
+//Date: 4-16-19
+//Purpose: Algortihm Workbench chapter 10 #6
+//a superclass has an abstract method called getValue(), write a getValue method in the subclass
+
+public abstract class Get_Value_Workbench {
+
+   public int n = 100;
+   
+   public abstract int getValue();
+   
+}
---
+++ Get_Value_Workbench_Demo.java
@@ -0,0 +1,13 @@
+//Name: Asia Minor
+//Date: 4-16-19
+//Purpose: Algortihm Workbench chapter 10 #6
+//a superclass has an abstract method called getValue(), write a getValue method in the subclass
+
+public abstract class Get_Value_Workbench_Demo extends Get_Value_Workbench {
+
+   @Override
+   public int getValue(){
+      return super.n;
+   }
+   
+}
